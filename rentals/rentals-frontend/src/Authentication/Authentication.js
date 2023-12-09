c5fdc9fb125c77268c02237a3b2a113b1d626f34
Cosmin Cosan <cosan_cosmin@yahoo.com>
2023-12-09 18:10:13 +0200

    some fixes on auth page

--- rentals/rentals-frontend/src/Authentication/Authentication.js
+++ rentals/rentals-frontend/src/Authentication/Authentication.js
@@ -1,48 +1,78 @@
 import './Authentication.css';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
+import React, {useState} from 'react';
 
 function EmailForm() {
-    return (
-        <div className = {"email-form"} >
-            <Form.Label>
-                Email address
-            </Form.Label>
+    // Define state to keep track of the email and password input values
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-            <span className = {"space-between-label-and-text"} />
+    // Event handler for email input changes
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    // Event handler for password input changes
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    // Function to handle form submission
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email entered:', email);
+        console.log('Password entered:', password);
+    };
+
+    return (
+        <form className="email-form" onSubmit={handleSubmit}>
+            <Form.Label>Email</Form.Label>
+            <div className="bottom-space-5"/>
 
             <Form.Control
-                type = "email"
+                type="email"
                 placeholder="name@example.com"
+                style={{
+                    fontSize: '15px',
+                    height: '30px',
+                }} value={email}
+                onChange={handleEmailChange}
             />
-        </div>
-    );
-}
 
-function PasswordForm() {
-    return (
-        <div className = "password-form" >
-            <Form.Label>
-                Password
-            </Form.Label>
 
-            <span className={"space-between-label-and-text"}/>
+            <div className="bottom-space-20"/>
 
+            <Form.Label>Password</Form.Label>
+            <div className="bottom-space-5"/>
             <Form.Control
                 type="password"
-                placeholder="qwert1234"
+                placeholder="enter your password"
+                style={{
+                    fontSize: '15px',
+                    height: '30px',
+                }}
+                value={password}
+                onChange={handlePasswordChange}
             />
-        </div>
-    );
-}
 
-function ContinueButton() {
-    return (
-        <div className={"space-left"}>
-            <button>
+            <div className="bottom-space-20"/>
+
+            <button
+                type="submit"
+                style={{
+                    height: '40px',
+                    width: '150px',
+                    borderRadius: '18px',
+                    background: 'darkblue',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    fontSize: '15px', // Set to a value that suits your design
+                }}
+            >
                 Continue
             </button>
-        </div>
+        </form>
     );
 }
 
@@ -50,8 +80,6 @@ function VStack() {
     return (
         <Stack gap={3}>
             <EmailForm/>
-            <PasswordForm/>
-            <ContinueButton/>
         </Stack>
     );
 }
